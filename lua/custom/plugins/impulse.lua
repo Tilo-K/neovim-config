8c626092bc51fb72624ec205e16be3391690cc4e
Tilo K <tiloklarenbeek@gmail.com>
2024-01-01 20:36:53 +0100

    Added fugitve removed impulse.

--- lua/custom/plugins/impulse.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "chrsm/impulse.nvim",
-    version = "*",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim", },
-    config = function()
-      require("impulse").setup( {api_key="secret_dlT76iEObyGwCLyedqct1PKlspozXFtS0T3ckpUWtZH"} )
-    end,
-}
