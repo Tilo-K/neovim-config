b280571d1cc3cdecf4976b295d416fd0226e375a
Tilo K <tiloklarenbeek@gmail.com>
2024-01-11 21:40:51 +0100

    I can do better

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,10 +1,11 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-}
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   version = "*",
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   config = function()
+--     require("nvim-tree").setup {}
+--   end,
+-- }
+-- NO!
