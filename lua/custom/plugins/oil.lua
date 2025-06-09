9eccb3143cecb01b8b7404f86a7fdddf3dc33797
Tilo K <tiloklarenbeek@gmail.com>
2025-06-09 13:26:22 +0200

    wip

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,7 +3,14 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == '..'
+        end,
+      },
+    },
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
